6c9b59c48b5d453f20e02174b26b931731ae0cc4
biro23 <121694408+biro23@users.noreply.github.com>
2024-01-01 21:52:10 -0300

    first commit of the project

---
+++ main.js
@@ -0,0 +1,43 @@
+// / criar um evento logo apos carregar todo documento HTML
+// // recuperando agora o botao, document.getElementaryid
+// document.addEventListener('DOMContentLoaded', function(){
+//   document.getElementById('btn-buscar-cep').addEventListener('click', function(){
+//     // AJAX - Asychronous JavaScript and XML
+
+//     const xhttp = new XMLHttpRequest();
+//     const cep = document.getElementById('cep').value;
+//     const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+//     xhttp.open('GET', endpoint);
+//     xhttp.send();
+
+//     // https://viacep.com.br/ws/1823000/json
+
+//   })
+// })
+
+$(document).ready(function () {
+  $("cep").mask("00000-000");
+
+  $("#btn-buscar-cep").click(function () {
+    const cep = $("#cep").val();
+    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+    const botao = $(this);
+    $("botao").find("i").addClass("d-none");
+    $("botao").find("span").removeClass("d-none");
+
+    $.ajax(endpoint).done(function (resposta) {
+      const logradouro = resposta.logradouro;
+      const bairro = resposta.bairro;
+      const cidade = resposta.localidade;
+      const estado = resposta.uf;
+      const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+      $("#endereco").val(endereco);
+
+      setTimeout(function () {
+        $("botao").find("i").removeClass("d-none");
+        $("botao").find("span").addClass("d-none");
+      }, 4000);
+    });
+  });
+});
